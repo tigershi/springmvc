0b7fc71e8cfd09e40ec813f4d5227e39912b65a7
tiger shi <tigersh123@163.com>
2012-09-10 18:26:29 +0800

    0910
    add the scheduling job

--- sale8qw/src/cn/djel/sale8/controller/test/PersonController.java
+++ sale8qw/src/cn/djel/sale8/controller/test/PersonController.java
@@ -20,27 +20,67 @@ public class PersonController {
 	public void setPersonService(IPersonService personService) {
 		this.personService = personService;
 	}
-
+	
+	@RequestMapping("/person/toAdd")
+    public String toAddPerson(){
+    	
+		return"/text/add";
+    }
+	
 	@RequestMapping("/person/add")
-	public String insertperson(HttpServletRequest request ,HttpServletResponse response) {
-		String name = request.getParameter("name");
-		String age = request.getParameter("age");
-		String sex = request.getParameter("sex");
-		String password = request.getParameter("password");
-		
-		System.out.println(name+age+sex+password);
-		Person p = new Person();
-		p.setName("ssssa");
-		p.setPassword("sssa");
-		p.setAge(223);
-		p.setSex("falemana");
-		personService.insertPerson(p);
-		// personService.insertPerson(model);
+	public String addperson(HttpServletRequest request ,HttpServletResponse response) {
+	//	System.out.println(name+age+sex+password);
+//		Person p = new Person();
+//		p.setName("ssssa");
+//		p.setPassword("sssa");
+//		p.setAge(223);
+//		p.setSex("falemana");
+//以上代码做测试		
+		Person per = assemPerson(request);
+		personService.insertPerson(per);
 		List<Person> li = personService.getAllPerson();
-		System.out.println(li.size());
+	//	System.out.println(li.size());
 		return "/test/add";
 
 	}
-   
+	public String edit(){
+		return null;
+		
+	}
+	public String update(){
+		return null;
+		
+	}
+	public String listPerson(){
+		return null;
+		
+	}
+	
+   private Person assemPerson(HttpServletRequest request ){
+	 Person per = new Person();
+	    String id = request.getParameter("id");   
+	    if(id !=null && !id.equals("")){
+	    	per.setId(Integer.parseInt(id));
+	    }
+		String name = request.getParameter("name");
+		if(name != null && !name.equals("")){
+			per.setName(name);
+		}
+		String age = request.getParameter("age");
+		if(age !=null && !age.equals("")){
+			per.setAge(Integer.parseInt(age));
+		}
+		String sex = request.getParameter("sex");
+		if(sex != null && !sex.equals("")){
+			per.setSex(sex);
+		}
+		String password = request.getParameter("password");
+		if(password != null && !password.equals("")){
+			per.setPassword(password);
+		}
+	   
+	   return per;
+	   
+   }
 
 }
---
+++ sale8qw/src/cn/djel/sale8/service/test/job/TestJob.java
@@ -0,0 +1,7 @@
+package cn.djel.sale8.service.test.job;
+
+public class TestJob {
+  public void execute(){
+	  System.out.print("helloword");
+  }
+}
